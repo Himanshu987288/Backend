c3a29fca1e0e31ebb1ba1f4cd4d5c7d604cd218d
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-04-04 14:23:43 +0530

    gb

---
+++ practice_debuging.js
@@ -0,0 +1,16 @@
+function calculationArea(width, height){
+    return (width + height);
+};
+
+let width = 10 ;
+let height = 5;
+if (area > 100){
+    console.log("the areais large");
+
+}else{
+    console.log("the area is small");
+
+}
+if (width + height > 100){
+    console.log("area is greater than or equal to 100");
+}
