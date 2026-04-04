da8b94354d8d00e00430e38673c8616588f7957d
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-04-04 14:19:24 +0530

    runtime

---
+++ runtime.js
@@ -0,0 +1,16 @@
+
+// runtime error acc to app2.js
+const runtime=()=>{
+    console.log("x is ");
+
+};
+module.exports=runtime;
+
+
+//logical error
+
+const logical=()=>{
+    console.log("it is a error bassed poblem");
+
+};
+module.exports= logical;
