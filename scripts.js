107589ee73cad045a0ca11cf6dcac0deb9872b75
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-04-04 14:20:27 +0530

    script

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// const somevalue = require("./math");
+// console.log(someValue);
+
+console.log(process.argv);
+//'C:\\Program Files\\nodejs\\node.exe',
+ // 'c:\\Users\\NCL\\OneDrive\\Desktop\\NodeJs\\scripts.js'
+ 
