d10cf4a24a2536f2b2458d590a0fb1a9ac6a6e0c
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-04-04 14:22:42 +0530

    process

---
+++ process.js
@@ -0,0 +1,3 @@
+//The obj provides info abt,and cntrl over, the current node.js process
+
+//process.argv:----> return an array contaning the command-line arguments passed when the node.js process was launched.
