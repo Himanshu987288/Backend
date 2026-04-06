013d0cb2a1cd18e77042a9861966d332bf403019
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-04-06 23:19:48 +0530

    model

---
+++ models.js
@@ -0,0 +1,9 @@
+const apple =require("./apple");
+const banana=require("/banana");
+const orange = require("./orange");
+
+let friuts=[apple, banana , orange];
+models.exports=nodejs//(exports to files )
+
+//require()----> a built-in function to include external modules that exits in separates files
+//module.exports----> a special obj
