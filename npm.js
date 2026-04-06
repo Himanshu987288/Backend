057daf9dd41691b34d5bcddabc1b9ddfff258f5f
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-04-06 23:21:53 +0530

    npm

--- npm.js
+++ npm.js
@@ -1,3 +1,8 @@
 // npm is the standard package manager for node.js
 //linrary of packages --> express.js and react.js(for front end)
 // use of npm :-install direct packages and dovelpoers direct use of codes help to npm
+
+//node.files------->
+//this code execute to script file acc if code in node.js file .
+
+//node REPL---->read, evaluate , print , loop
