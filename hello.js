984356239b491c36c210f1dab21d65366f594a00
Himanshu987288 <dhimanhimanshu9520@gmail.com>
2026-05-18 10:35:40 +0530

    hello nodejs

---
+++ hello.js
@@ -0,0 +1,7 @@
+// console.log("hello world");
+
+// console.log("hi there i am himanshu");
+
+
+const math = require('./math1');// ./ current working directory
+console.log("math value is a",math(3,6));
